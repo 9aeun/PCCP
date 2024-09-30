1b21fafecaf26a57c95e72c0897083a2d239aecb
9aeun <rlarkdms0424@naver.com>
2024-09-30 10:45:55 +0900

    aa

--- src/main/java/com/pccp_1_문자열/_1_String.java
+++ src/main/java/com/pccp_1_문자열/_1_String.java
@@ -1,5 +1,9 @@
 package com.pccp_1_문자열;
 
+import java.sql.Array;
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
 public class _1_String {
     public static void main(String[] args){
         String word = "university"; //할당
@@ -23,5 +27,41 @@ public class _1_String {
             System.out.println(word.charAt(i));
             i+=1;   //i++ 대신 i+=1로 사용하기 (clean code)
         }
+
+        // contains -> boolean , if문이랑 많이 사용
+        word = "banana";
+
+        System.out.println(word.contains("n")); // true
+        System.out.println(word.contains("nana"));  // true
+        System.out.println(word.contains("f")); // false
+
+        // replace
+        word = "banana";
+        System.out.println(word.replace("a", "c")); // bcncnc
+        System.out.println(word.replace("ba", "fff"));
+        System.out.println(word.replace("b", ""));  // 특정 문자열을 지우는 것처럼 대체
+        System.out.println(word);   // banana => immutable(int, float, double, String, ...의 기본자료형)들은 변경X , 새롭게 (반대 : mutable)
+
+        // split
+        String sentence = "hello world !";
+        String sentence1 = "banana";
+        String[] strings = sentence.split(" "); // 구분자를 기준으로 문자열을 자름
+        String[] as = sentence1.split("a");
+        System.out.println(Arrays.toString(strings));   // [hello, world, !]
+        System.out.println(Arrays.toString(as));    // [b, n, n]
+
+        // join
+        String joinedSentence = String.join(" ", strings);
+        System.out.println(joinedSentence); // "hello world !"
+
+        // equals
+        String a = "banana";
+        String b = "banana";
+        String c = "apple";
+
+        System.out.println(a.equals(b));    // true
+        System.out.println(b.equals(a));    // true
+        System.out.println(a.equals(c));    // false
+        // == 은 정수비교, String에서 사용하면 X
     }
 }
